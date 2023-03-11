35ecd6034fe4108bfb54a916394f7b784d4de1e8
sofianicolae <sofianicolae88@gmail.com>
2023-03-11 14:30:33 +0200

    Refactored code -- renaming classes and interfaces & correcting typos

--- src/ro/ase/acs/classes/DatabaseDatabaseConnection.java
+++ src/ro/ase/acs/classes/DatabaseDatabaseConnection.java
@@ -1,12 +1,12 @@
 package ro.ase.acs.classes;
 
-import ro.ase.acs.interfaces.ConnectionData;
+import ro.ase.acs.interfaces.DatabaseConnectionDefinition;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DatabaseConnection implements ConnectionData {
+public class DatabaseDatabaseConnection implements DatabaseConnectionDefinition {
     @Override
     public Connection connectionDefinition() throws SQLException, ClassNotFoundException {
         Class.forName("org.sqlite.JDBC");
